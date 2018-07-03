d429ea10cf4933551796f07ea6d61bf54ad475d4
Abhishek <abhishekbphc922@gmail.com>
2018-07-03 18:29:42 +0530

    Committing Titanic project

---
+++ titanic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Titanic survival prediction',
+    author='Abhishek_Alluri',
+    license='MIT',
+)
